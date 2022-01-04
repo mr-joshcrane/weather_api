6a170c7abad79789a40e924909a9ba8819506e3e
Josh Crane <josh.crane@mechanicalrock.io>
2022-01-04 13:12:48 +0800

    Initial commit

---
+++ weather.go
@@ -0,0 +1,116 @@
+package weather
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Temperature float64
+
+type Conditions struct {
+	Summary     string
+	Temperature Temperature
+}
+
+type OWMResponse struct {
+	Weather []struct {
+		Main string
+	}
+	Main struct {
+		Temp Temperature
+	}
+}
+
+type Client struct {
+	APIKey     string
+	BaseURL    string
+	HTTPClient *http.Client
+}
+
+func ParseResponse(data []byte) (Conditions, error) {
+	var resp OWMResponse
+	err := json.Unmarshal(data, &resp)
+	if err != nil {
+		return Conditions{}, fmt.Errorf("invalid API response %s: %w", data, err)
+	}
+	if len(resp.Weather) < 1 {
+		return Conditions{}, fmt.Errorf("invalid API response %s: require at least one weather element", data)
+	}
+	conditions := Conditions{
+		Summary:     resp.Weather[0].Main,
+		Temperature: resp.Main.Temp,
+	}
+	return conditions, nil
+}
+
+func FormatURL(baseURL, location, key string) string {
+	return fmt.Sprintf("%s/data/2.5/weather?q=%s&appid=%s", baseURL, location, key)
+}
+
+func NewClient(key string) *Client {
+	return &Client{
+		APIKey:  key,
+		BaseURL: "https://api.openweathermap.org",
+		HTTPClient: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+func (c *Client) FormatURL(location string) string {
+	return fmt.Sprintf("%s/data/2.5/weather?q=%s&appid=%s", c.BaseURL, location, c.APIKey)
+}
+
+func (c *Client) GetWeather(location string) (Conditions, error) {
+	URL := c.FormatURL(location)
+	resp, err := c.HTTPClient.Get(URL)
+	if err != nil {
+		return Conditions{}, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return Conditions{}, fmt.Errorf("unexpected response status %q", resp.Status)
+	}
+	data, err := io.ReadAll(resp.Body)
+	conditions, err := ParseResponse(data)
+	if err != nil {
+		return Conditions{}, err
+	}
+	return conditions, nil
+}
+
+func Get(location, key string) (Conditions, error) {
+	c := NewClient(key)
+	conditions, err := c.GetWeather(location)
+	if err != nil {
+		return Conditions{}, err
+	}
+	return conditions, nil
+}
+
+func RunCLI() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s LOCATION\n\nExample: %[1]s London,UK", os.Args[0])
+		os.Exit(1)
+	}
+	location := os.Args[1]
+	key := os.Getenv("OPENWEATHERMAP_API_KEY")
+	if key == "" {
+		fmt.Fprintln(os.Stderr, "Please set the environment variable OPENWEATHERMAP_API_KEY")
+		os.Exit(1)
+	}
+	conditions, err := Get(location, key)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s %.1fº\n", conditions.Summary, conditions.Temperature.Celcius())
+}
+
+func (t Temperature) Celcius() float64 {
+	return float64(t) - 273.15
+}
